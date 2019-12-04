fbc2472e0393126c37a820312aba95eed9b8ffdb
Andy Chai <andyaustinchai@gmail.com>
2019-12-04 00:04:07 -0600

    hi

--- frontend/GroceryBuddy/SearchIngredientViewController.swift
+++ frontend/GroceryBuddy/SearchIngredientViewController.swift
@@ -12,10 +12,53 @@ class SearchIngredientViewController: UIViewController {
     
     var username:String = ""
     
+    fileprivate let collectionView:UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        layout.minimumInteritemSpacing = 50
+        layout.minimumLineSpacing = 5
+        return cv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.title = "Browse Recipes"
+
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .white
         self.view.backgroundColor = .white
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive=true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5).isActive=true
+        collectionView.trailingAnchor.constraint(equalTo:view.trailingAnchor,constant: -5).isActive=true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive=true
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
     }
 }
+
+extension SearchIngredientViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/2.5, height: collectionView.frame.width/2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+        //return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) //as! CustomCell
+        //cell.data = self.data[indexPath.item]
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+}
+
+ 
